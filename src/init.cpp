381fe8a92dc11b567d1a35734064e01feab71095
daniel <daniel.socials@gmail.com>
2015-11-12 09:53:59 +0800

    delete all utx when start
    Conflicts:
    
    	src/dblayer.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -400,6 +400,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += "  -dbport=<port>\n";
     strUsage += "  -dbuser=<username>\n";
     strUsage += "  -dbpass=<password>\n";
+    strUsage += "  -deleteallutx=<true>\n";
     return strUsage;
 }
 
@@ -1068,6 +1069,15 @@ bool AppInit2(boost::thread_group& threadGroup)
                 }
 
                 uiInterface.InitMessage(_("dbSync begin..."));
+
+                bool deleteallutx = GetArg("-deleteallutx", true);
+                if  (deleteallutx) {
+                    if (dbDeleteAllUtx() == -1) {
+                        strLoadError = _("Error delete all utx from database...");
+                        break;
+                    }
+                }
+ 
                 if (dbSync() == -1) {
                     strLoadError = _("Error sql database sync...");
                     break;
