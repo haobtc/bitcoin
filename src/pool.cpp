6928cd9180a5c37ec8b87ea23128224951726a0b
daniel <daniel.socials@gmail.com>
2015-11-18 09:29:04 +0800

    add Nexious pool

--- src/pool.cpp
+++ src/pool.cpp
@@ -84,6 +84,7 @@ static struct PoolAddr poolAddresses[] = {
     {"1MeffGLauEj2CZ18hRQqUauTXb9JAuLbGw", POOL_MULTIPOOL},
     {"1qtKetXKgqa7j1KrB19HbvfRiNUncmakk",  POOL_TRANSACTIONCOINMINING},
     {"1MFsp2txCPwMMBJjNNeKaduGGs8Wi1Ce7X", POOL_A_XBT},
+    {"1GBo1f2tzVx5jScV9kJXPUP9RjvYXuNzV7", POOL_NEXIOUS},
     {"1KTNEBhQdj81UhDkJaoe3r8VtAsrnmpmnS", POOL_UNKNOWN},
     {"1Ar2gRkt1u6k4PToeeTKm1KGmtR2GRA1wL", POOL_UNKNOWN},
     {"18UBaMPq6FQfXnKT19rXc8bFFFwQz52Bc1", POOL_UNKNOWN},
@@ -193,6 +194,7 @@ static struct PoolPrefix poolPrefixes[] = {
     {"wizkid057",POOL_WIZKID057},
     {"HASHPOOL",POOL_HASHPOOL},
     {"/A-XBT/",POOL_A_XBT},
+    {"/Nexious/", POOL_NEXIOUS},
     {"p2pool", POOL_P2POOL}
 };
 
