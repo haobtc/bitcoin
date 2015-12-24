5e85d2dd6fcc63313bacd69ccd4c985dfb31f18b
daniel <daniel.socials@gmail.com>
2015-12-24 09:13:03 +0800

    add Tricky's BTC Pool
    Conflicts:
    
    	src/pool.h

--- src/pool.h
+++ src/pool.h
@@ -83,6 +83,8 @@ enum POOL_TYPE {
     POOL_TRANSACTIONCOINMINING = 106, //"http://sha256.transactioncoinmining.com/"
     POOL_A_XBT = 107, //"http://www.a-xbt.com/"
     POOL_NEXIOUS = 108, //https://nexious.com/
+    POOL_BRAVO =109,    //http://www.bravo-mining.com/
+    POOL_POOL_TRICKYS =110,    //http://pool.wemine.uk/
 };
  
  
