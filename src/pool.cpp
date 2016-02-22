2778335dcab730deb661bd442a50065c91333f8d
daniel <daniel.socials@gmail.com>
2016-02-22 08:44:05 +0800

    add Eobot pool

--- src/pool.cpp
+++ src/pool.cpp
@@ -97,6 +97,7 @@ static struct PoolAddr poolAddresses[] = {
     {"1GBo1f2tzVx5jScV9kJXPUP9RjvYXuNzV7", POOL_NEXIOUS},
     {"1AePMyovoijxvHuKhTqWvpaAkRCF4QswC6", POOL_TRICKYS},
     {"19f8Pk3m1GySyowWb8qLy93R5ngAUoVoUE", POOL_MANHATTANMINE},
+    {"1MPxhNkSzeTNTHSZAibMaS8HS1esmUL1ne", POOL_EOBOT},
     {"1KTNEBhQdj81UhDkJaoe3r8VtAsrnmpmnS", POOL_UNKNOWN},
     {"1Ar2gRkt1u6k4PToeeTKm1KGmtR2GRA1wL", POOL_UNKNOWN},
     {"18UBaMPq6FQfXnKT19rXc8bFFFwQz52Bc1", POOL_UNKNOWN},
