d63c97a77626074ed227810bc5f75e98cccba914
daniel <daniel.socials@gmail.com>
2016-04-05 15:03:07 +0800

    fix lock issue

--- src/init.cpp
+++ src/init.cpp
@@ -1390,24 +1390,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         mempool.ReadFeeEstimates(est_filein);
     fFeeEstimatesInitialized = true;
 
-    if (GetArg("-savemempool", true)) {
-        // It is OK if mempool.Read() fails; starting out with an empty memory pool is not
-        // a problem, it gets filled quickly.
-        list<CTxMemPoolEntry> mempoolEntries;
-        if (mempool.Read(mempoolEntries) && !(mempoolEntries.empty()))
-        {
-            CValidationState valState;
-            bool fMissingInputs;
-            BOOST_FOREACH(CTxMemPoolEntry& mempoolEntry, mempoolEntries)
-            {
-                AcceptToMemoryPool(mempool, valState, mempoolEntry.GetTx(), false,
-                                   &fMissingInputs, false);
-            }
-            LogPrintf("Accepted %lu mempool transactions\n", mempool.size());
-        }
-    }
-
-
     // ********************************************************* Step 8: load wallet
 #ifdef ENABLE_WALLET
     if (fDisableWallet) {
