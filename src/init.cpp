c7f8927ab83a422f9077195c6f1a4744d2ad090f
daniel <daniel.socials@gmail.com>
2016-04-05 15:03:07 +0800

    fix lock issue

--- src/init.cpp
+++ src/init.cpp
@@ -1435,7 +1435,26 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         while (!fRequestShutdown && chainActive.Tip() == NULL)
             MilliSleep(10);
     }
+ 
+    if (GetArg("-savemempool", true)) {
 
+        LOCK(cs_main);
+        // It is OK if mempool.Read() fails; starting out with an empty memory pool is not
+        // a problem, it gets filled quickly.
+        list<CTxMemPoolEntry> mempoolEntries;
+        if (mempool.Read(mempoolEntries) && !(mempoolEntries.empty()))
+        {
+            CValidationState valState;
+            bool fMissingInputs;
+            BOOST_FOREACH(CTxMemPoolEntry& mempoolEntry, mempoolEntries)
+            {
+                AcceptToMemoryPool(mempool, valState, mempoolEntry.GetTx(), false,
+                                   &fMissingInputs, false);
+            }
+            LogPrintf("Accepted %lu mempool transactions\n", mempool.size());
+        }
+    }
+ 
     // ********************************************************* Step 11: start node
 
     if (!CheckDiskSpace())
