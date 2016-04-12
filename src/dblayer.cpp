30ac53d615d6f13f4ccba024998e1f8c4a583708
daniel <daniel.socials@gmail.com>
2016-04-12 10:22:29 +0800

    fix dbsync issue when db connect fail

--- src/dblayer.cpp
+++ src/dblayer.cpp
@@ -439,8 +439,7 @@ int dbSync(int newHeight) {
 
   if (maxHeight + 1 == newHeight)
   {
-    LogPrint("dblayer", "maxHeight in db: %d, newHeight: %d\n", maxHeight, newHeight);
-
+    syncing=false;
     return 0;
   }
 
@@ -451,6 +450,7 @@ int dbSync(int newHeight) {
     for (; i < (chainActive.Height() + 1); i++) {
 
       if (ShutdownRequested()) {
+        syncing=false;
         LogPrint("dblayer", "Shutdown requested. Exiting.");
         syncing=false;
         return -1;
