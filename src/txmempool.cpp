9c270337471e4c17744c7ac7d823bd4fe64f1a7f
daniel <daniel.socials@gmail.com>
2017-03-31 16:48:07 +0800

    remove from db before remove from mempool

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -997,9 +997,9 @@ void CTxMemPool::RemoveStaged(setEntries &stage, bool updateDescendants, MemPool
     AssertLockHeld(cs);
     UpdateForRemoveFromMempool(stage, updateDescendants);
     BOOST_FOREACH(const txiter& it, stage) {
-        removeUnchecked(it, reason);
         if (fRemoveFromDb)
             dbRemoveTx(it->GetTx().GetHash()); 
+        removeUnchecked(it, reason);
     }
 }
 
